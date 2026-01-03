ff2bb26669d8a17e91feb67a4a5231045aaec343
Rany Daioub <royrootdev@proton.me>
2026-01-03 16:14:36 +1100

    fix: players flex layout

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,24 @@ function App() {
     <div id="game-container">
       <section id="board-container"></section>
       <section id="info-container">
-        <div id="players-container"></div>
+        <div id="players-container">
+          <ol>
+            <li className="player active">
+              <span className="player-name">
+                <span className="text">Player 1</span>
+                <span className="player-symbol">X</span>
+              </span>
+              <button>Edit</button>
+            </li>
+            <li className="player">
+              <span className="player-name">
+                <span className="text">Player 2</span>
+                <span className="player-symbol">O</span>
+              </span>
+              <button>Edit</button>
+            </li>
+          </ol>
+        </div>
         <div id="logs-container"></div>
       </section>
     </div>
