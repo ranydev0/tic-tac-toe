b42bb368e60fd9847d75d8801afd9787b1be6418
Rany Daioub <royrootdev@proton.me>
2026-01-03 14:16:35 +1100

    fix: responsive layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,10 @@
 function App() {
-  return <div></div>;
+  return (
+    <div id="game-container">
+      <div id="board-container"></div>
+      <div id="info-container"></div>
+    </div>
+  );
 }
 
 export default App;
