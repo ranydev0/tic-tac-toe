c08fc0c1dabee9800b6ee4209f2434c25399553b
Rany Daioub <royrootdev@proton.me>
2026-01-04 15:44:37 +1100

    fix: board grid

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,23 @@
 function App() {
+  const initialGameBoard = [
+    [null, null, null],
+    [null, null, null],
+    [null, null, null],
+  ];
+
   return (
     <div id="game-container">
-      <section id="board-container"></section>
+      <section id="board-container">
+        <ol id="game-board">
+          {initialGameBoard.map((row, rowIndex) =>
+            row.map((col, colIndex) => (
+              <li key={`${rowIndex}-${colIndex}`}>
+                <button className="game-button">{col}</button>
+              </li>
+            ))
+          )}
+        </ol>
+      </section>
       <section id="info-container">
         <div id="players-container">
           <ol>
